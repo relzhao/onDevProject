1e084cbc140eadf7e7d8d1091005d9e1ffe27c59
rel zhao <shiliu.as.me@gmail.com>
2021-06-08 00:13:09 +0800

    6/8

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,17 +1,19 @@
 <!--
  * @Author: your name
  * @Date: 2021-05-25 23:04:02
- * @LastEditTime: 2021-06-07 17:42:10
+ * @LastEditTime: 2021-06-08 00:11:22
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /onDevProject/src/components/breadcrumb/index.vue
 -->
 <template>
   <el-breadcrumb separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    <el-breadcrumb-item
+      :to="{ path: '/' }"
+      v-for="item in breadList"
+      :key="item.path"
+      >{{ item.meta.title }}</el-breadcrumb-item
+    >
   </el-breadcrumb>
 </template>
 
@@ -22,6 +24,15 @@ export default {
       breadList: null,
     };
   },
+  watch: {
+    //监听到路由地址变化后重新调用getBreadcrumb() 如果地址为重定向地址则忽略
+    $route(to) {
+      if (to.path.startsWith("/redirect")) {
+        return;
+      }
+      this.getBreadcrumb();
+    },
+  },
   created() {
     this.getBreadcrumb();
   },
@@ -30,8 +41,16 @@ export default {
       let matched = this.$route.matched.filter(
         (item) => item.meta && item.meta.title
       );
-      const first = matched[0];
-      console.log(first);
+      const first = matched[0]; //取第一个route对象 用于判断其是否为dashboard
+      if (!this.isDashboard(first)) {
+        matched = [{ path: "/dashboard", meta: { title: "Dashboard" } }].concat(
+          matched
+        );
+      }
+      this.breadList = matched.filter(
+        (item) => item.meta && item.meta.title !== false
+      );
+      console.log(this.breadList);
     },
     isDashboard(route) {
       const name = route && route.name;
@@ -46,5 +65,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
